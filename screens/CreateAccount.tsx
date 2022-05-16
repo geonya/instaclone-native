0e21136af7a562d68b0f1f3078c24ebb5bf921ae
geonya <geony8410@gmail.com>
2022-05-16 16:36:37 +0900

    #14.1 Stack Navigator Typescript

--- screens/CreateAccount.tsx
+++ screens/CreateAccount.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { Text, TouchableOpacity, View } from "react-native";
+import { StackParamList } from "../navigators/LoggedOutNav";
 
-const CreateAccount = () => {
+type CreateAccountScreenProps = NativeStackScreenProps<
+	StackParamList,
+	"CreateAccount"
+>;
+
+const CreateAccount = ({ navigation }: CreateAccountScreenProps) => {
 	return (
 		<View>
 			<Text>CreateAccount</Text>
+			<TouchableOpacity onPress={() => navigation.navigate("Login")}>
+				<View>
+					<Text>Go to Login</Text>
+				</View>
+			</TouchableOpacity>
 		</View>
 	);
 };
