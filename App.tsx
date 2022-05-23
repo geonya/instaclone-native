591ea4f8e674637decbf5ccc3337e674737b6266
geonya <geony8410@gmail.com>
2022-05-23 14:42:16 +0900

    expor-app-loading error -> install SplashScreen

--- App.tsx
+++ App.tsx
@@ -1,24 +1,20 @@
-import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
 import { Asset } from "expo-asset";
 import { Ionicons } from "@expo/vector-icons";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import LoggedOutNav from "./navigators/LoggedOutNav";
 import { NavigationContainer } from "@react-navigation/native";
 import { ApolloProvider, useReactiveVar } from "@apollo/client";
 import client, { cache, isLoggedInVar, tokenVar } from "./apollo";
 import LoggedInNav from "./navigators/LoggedInNav";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {
-	AsyncStorageWrapper,
-	CachePersistor,
-	persistCache,
-} from "apollo3-cache-persist";
+import { AsyncStorageWrapper, CachePersistor } from "apollo3-cache-persist";
+import * as SplashScreen from "expo-splash-screen";
+import { View } from "react-native";
 
 export default function App() {
 	const isLoggedIn = useReactiveVar(isLoggedInVar);
-	const [loading, setLoading] = useState(true);
-	const onFinish = () => setLoading(false);
+	const [appIsReady, setAppIsReady] = useState(false);
 	const preLoadAssets = () => {
 		const fontsToLoad = [Ionicons.font];
 		const fontPromises = fontsToLoad.map((font: any) => Font.loadAsync(font));
@@ -28,38 +24,47 @@ export default function App() {
 		);
 		Promise.all([...fontPromises, ...imagePromises]);
 	};
-	const preLoad = async () => {
-		const token = await AsyncStorage.getItem("token");
-		const cachePersistor = new CachePersistor({
-			cache,
-			storage: new AsyncStorageWrapper(AsyncStorage),
-		});
-		if (token) {
-			isLoggedInVar(true);
-			tokenVar(token);
+
+	useEffect(() => {
+		const preLoad = async () => {
+			const token = await AsyncStorage.getItem("token");
+			const cachePersistor = new CachePersistor({
+				cache,
+				storage: new AsyncStorageWrapper(AsyncStorage),
+			});
+			if (token) {
+				isLoggedInVar(true);
+				tokenVar(token);
+			}
+			try {
+				await SplashScreen.preventAutoHideAsync();
+				preLoadAssets();
+				await cachePersistor.restore();
+				// purge() 시 저장된 cache 모두 삭제
+			} catch (err) {
+				console.error(err);
+			} finally {
+				setAppIsReady(true);
+			}
+		};
+		preLoad();
+	}, []);
+
+	const onLayoutRootView = useCallback(async () => {
+		if (appIsReady) {
+			await SplashScreen.hideAsync();
 		}
-		try {
-			// await cachePersistor.restore();
-			// purge() 시 저장된 cache 모두 삭제
-		} catch (err) {
-			console.error(err);
-		}
-		return preLoadAssets();
-	};
-	if (loading) {
-		return (
-			<AppLoading
-				startAsync={preLoad}
-				onFinish={onFinish}
-				onError={console.error}
-			/>
-		);
+	}, [appIsReady]);
+	if (!appIsReady) {
+		return null;
 	}
 	return (
-		<ApolloProvider client={client}>
-			<NavigationContainer>
-				{isLoggedIn ? <LoggedInNav /> : <LoggedOutNav />}
-			</NavigationContainer>
-		</ApolloProvider>
+		<View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+			<ApolloProvider client={client}>
+				<NavigationContainer>
+					{isLoggedIn ? <LoggedInNav /> : <LoggedOutNav />}
+				</NavigationContainer>
+			</ApolloProvider>
+		</View>
 	);
 }
